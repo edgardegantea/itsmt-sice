f4a93275138285f2ac58ea7e27950aa5cbc753f3
Edgar Degante-Aguilar <edegantea@Edgars-MacBook-Pro.local>
2026-06-21 16:51:43 -0600

    fix: Corrección de PDFs 17

--- backend/resources/views/pdfs/partials/header.blade.php
+++ backend/resources/views/pdfs/partials/header.blade.php
@@ -54,13 +54,13 @@
       <td class="header-center">
         <h1>{{ $cfg->nombre_institucion }}</h1>
         <!-- <p>{{ $cfg->dependencia ?? 'Tecnológico Nacional de México' }} · {{ now()->format('d/m/Y') }}</p> -->
-        @if($cfg->clave_tecnm)<strong>{{ $cfg->clave_tecnm }}</strong><br>@endif
+        @if($cfg->clave_tecnm)<strong>{{ $cfg->clave_tecnm }}</strong> | @endif
         @if($cfg->ciudad){{ $cfg->ciudad }}{{ $cfg->estado ? ', ' . $cfg->estado : '' }}@endif
       </td>
-      <td class="header-right">
+      <!-- <td class="header-right">
         @if($cfg->clave_tecnm)<strong>{{ $cfg->clave_tecnm }}</strong><br>@endif
         @if($cfg->ciudad){{ $cfg->ciudad }}{{ $cfg->estado ? ', ' . $cfg->estado : '' }}@endif
-      </td>
+      </td> -->
     </tr>
   </table>
 </div>
