093639285bf95afa1f48423f8d54ae04af1602aa
Edgar Degante-Aguilar <edegantea@Edgars-MacBook-Pro.local>
2026-06-23 17:07:26 -0600

    fix: Mayúsculas 4

--- backend/resources/views/pdfs/contrato_estudiante.blade.php
+++ backend/resources/views/pdfs/contrato_estudiante.blade.php
@@ -44,7 +44,7 @@
 <div style="display: flex; justify-content: space-between; margin-top: 30px;">
   <div class="firma-block">
     <div class="firma-line"></div>
-    <p style="font-size:8pt;">{{ mb_strtoupper($inscripcion->aspirante->nombres . ' '  . $inscripcion->aspirante->apellido_paterno . ' ' . $inscripcion->aspirante->apellido_paterno, 'UTF-8') }}</p>
+    <p style="font-size:8pt; font-weight:bold;">{{ mb_strtoupper($inscripcion->aspirante->nombres . ' '  . $inscripcion->aspirante->apellido_paterno . ' ' . $inscripcion->aspirante->apellido_paterno, 'UTF-8') }}</p>
     <p style="font-size:8pt;">Estudiante</p>
   </div>
   <div class="firma-block">
