55a14629d096627ed0232f8275607b75494e979c
Edgar Degante-Aguilar <edegantea@Edgars-MacBook-Pro.local>
2026-06-25 15:12:15 -0600

    feat: Gestión académica

--- frontend/dist/assets/pdfHelpers-BaJgdsX7.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-DEicO7Zv.js","assets/react-DoK4WR2u.js","assets/QueryClientProvider-DqUGAOul.js","assets/apiClient-q7smXMO0.js","assets/index-CcxWsQrS.css"])))=>i.map(i=>d[i]);
-import{h as e}from"./index-DEicO7Zv.js";function t(t,n){e(async()=>{let{usePdfPreviewStore:e}=await import(`./index-DEicO7Zv.js`).then(e=>e.i);return{usePdfPreviewStore:e}},__vite__mapDeps([0,1,2,3,4])).then(({usePdfPreviewStore:e})=>{e.getState().open(t,n)})}export{t};
